4eb5c1e476cf8cba80ff1a97e8b3c530d8377f56
docbronovan <brockdonovan@gmail.com>
2015-08-12 23:40:34 -0400

    everything but validation

--- app/controllers/registered_applications_controller.rb
+++ app/controllers/registered_applications_controller.rb
@@ -1,22 +1,33 @@
-class RegisteredController < Registered_applicationController
+class RegisteredApplicationsController < ApplicationController
   def index
-    @registered_applications = Registered_application.all
+    @registered_applications = RegisteredApplication.all
   end
 
   def show
-    @registered_application = Registered_application.find(params[:id])
+    @registered_application = RegisteredApplication.find(params[:id])
   end
 
   def new
-    @registered_application = Registered_application.new
+    @registered_application = RegisteredApplication.new
   end
 
   def edit
-    @registered_application = Registered_application.find(params[:id])
+    @registered_application = RegisteredApplication.find(params[:id])
+  end
+
+  def update
+    @registered_application = RegisteredApplication.find(params[:id])
+    if @registered_application.update_attributes(params.require(:registered_application).permit(:name,:url))
+       flash[:notice] = "App was updated."
+       redirect_to @registered_application
+     else
+       flash[:error] = "There was an error saving the app. Please try again."
+       render :edit
+     end
   end
 
   def create
-    @registered_application = Registered_application.new(params.require(:applications).permit(:name,:url))
+    @registered_application = RegisteredApplication.new(params.require(:registered_application).permit(:name,:url))
     @registered_application.user_id = current_user.id
     #@registered_applications = current_user.registered_applications.new(params.require(:registered_applications).permit(:name,:url))
     
@@ -30,7 +41,7 @@ class RegisteredController < Registered_applicationController
   end
 
   def destroy
-    @registered_application = Registered_application.find(params[:id])
+    @registered_application = RegisteredApplication.find(params[:id])
 
     if @registered_application.destroy
       flash[:notice] = "application was removed."
--- app/models/registered_application.rb
+++ app/models/registered_application.rb
@@ -1,3 +1,12 @@
-class Registered_application < ActiveRecord::Base
+class RegisteredApplication< ActiveRecord::Base
   belongs_to :user
+
+
+  validate :validate_url
+
+  private
+
+  def validate_url
+    errors.add(:url, "is taken") if RegisteredApplication.exists?(self.url)
+  end
 end
