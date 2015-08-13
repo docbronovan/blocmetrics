09b20143efa55ce030b3c7e882118cd8be9361a4
docbronovan <brockdonovan@gmail.com>
2015-08-13 08:04:15 -0400

    validate url

--- app/models/registered_application.rb
+++ app/models/registered_application.rb
@@ -2,11 +2,6 @@ class RegisteredApplication< ActiveRecord::Base
   belongs_to :user
 
 
-  validate :validate_url
+  validates :url, uniqueness: true
 
-  private
-
-  def validate_url
-    errors.add(:url, "is taken") if RegisteredApplication.exists?(self.url)
-  end
 end
