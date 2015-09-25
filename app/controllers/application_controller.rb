75f0dca553d7c7992daf12c230e37b246eab70e5
docbronovan <brockdonovan@gmail.com>
2015-09-24 23:13:00 -0400

    format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,23 +5,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   #before_action :configure_permitted_parameters, if: :devise_controller?
 
-
-  
-  #def after_sign_in_path_for(resource)
-  #  topics_path
-  #end
-
-  # before_filter :set_access_control_headers
-
-  # def set_access_control_headers
-  #   # At #1, we allow requests from any origin.
-  #   headers['Access-Control-Allow-Origin'] = '*'
-  #   #2, we permit the POST, GET, OPTIONS request methods.
-  #   headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-  #   #3, we allow the header Content-Type, which is used in HTTP requests to declare the type of the data being sent.
-  #   headers['Access-Control-Allow-Headers'] = 'Content-Type'
-  # end
-
   protected
 
   def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'welcome/index'
-  get 'welcome/about'
 
   root to: 'welcome#index'
   resources :registered_applications
